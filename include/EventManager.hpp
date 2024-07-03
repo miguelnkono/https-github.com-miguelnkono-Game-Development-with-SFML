770eb58a5988d707471c9237b5fd026fd3520041
Miguel <nkonondememiguel7@gmail.com>
2024-07-03 19:27:50 +0200

    Implementing the Event Manager class to deal with the event in the future.

--- include/EventManager.hpp
+++ include/EventManager.hpp
@@ -1,16 +1,19 @@
 #ifndef EVENTMANAGER_HPP
 #define EVENTMANAGER_HPP
 
+/*** Includes ***/
+#include <SFML/Window/Event.hpp>
+#include <SFML/Graphics.hpp>
 #include <vector>
 #include <unordered_map>
 #include <functional>
-#include <SFML/Graphics.hpp>
 
-/// Enumerations of the different type of events.
+/*** Data type ***/
+/// Enumerations of all the events we'll be dealing with.
 enum class EventType {
     KeyDown         = sf::Event::KeyPressed,
-    Key             = sf::Event::KeyReleased,
-    MButtonDown     = sf::Event::MouseButtonPressed,
+    KwyUp           = sf::Event::KeyReleased,
+    MButonDown      = sf::Event::MouseButtonPressed,
     MButtonUp       = sf::Event::MouseButtonReleased,
     MouseWheel      = sf::Event::MouseWheelMoved,
     WindowResized   = sf::Event::Resized,
@@ -23,85 +26,84 @@ enum class EventType {
     Keyboard        = sf::Event::Count + 1, Mouse, Joystick
 };
 
-/// Structure to store the informations about the keys been pressed.
+/// The event info structure.
 struct EventInfo {
     EventInfo() { m_code = 0; }
     EventInfo(int l_event) { m_code = l_event; }
-
     union {
         int m_code;
     };
 };
 
-/// Data type to hold the information on the event.
+/// Container to store all the events that acquired.
 using Events = std::vector<std::pair<EventType, EventInfo>>;
 
-/// Details about the event actually handling.
+/// Data type to share all the infos about any event with the rest of the code.
 struct EventDetails {
-    EventDetails(std::string& l_bindName)
+    EventDetails(const std::string& l_bindName)
         : m_name(l_bindName)
-    {
-        Clear();
-    }
+        {
+            Clear();
+        }
 
     std::string m_name;
     sf::Vector2i m_size;
     sf::Uint32 m_textEntered;
     sf::Vector2i m_mouse;
-    int m_keyCode;      /// Single key code.
+    int m_mouseWheelDelta;
+    int m_keyCode;      // single key.
 
-    void Clear()
-    {
+    void Clear() {
         m_size = sf::Vector2i(0, 0);
         m_textEntered = 0;
         m_mouse = sf::Vector2i(0, 0);
+        m_mouseWheelDelta = 0;
         m_keyCode = -1;
     }
 };
 
-/// Structure that is going  to hold all the event infos.
+/// The binding structure.
 struct Binding {
-    Binding(std::string l_name)
+    Binding(const std::string& l_name)
         : m_name(l_name), m_details(l_name), c(0) {}
 
-    void BindEvent(EventType l_type, EventInfo l_info = EventInfo())
+    void BindEvent (EventType l_type, EventInfo l_info)
     {
-        m_event.emplace_back(l_type, l_info);
+        m_events.emplace_back(l_type, l_info);
     }
 
-    Events m_event;
-    std::string m_name;
-    int c;      /// Count of events that are "happening".
-
+    Events m_events;
     EventDetails m_details;
-} ;
+    std::string m_name;
+    int c;  /// Count of events that are happening.
+};
 
-/// Data structure to hold all the bindings.
+/// Container to store all the bindings.
 using Bindings = std::unordered_map<std::string, Binding*>;
 
-/// Type of the callback container.
+/// Type of the callback pointer container.
 using Callbacks = std::unordered_map<std::string, std::function<void(EventDetails*)>>;
 
-
-/// The EventManager class.
-class EventManager {
+/*** Implementation of the class EventManager. ***/
+class EventManager
+{
 public:
     EventManager();
     ~EventManager();
 
-    bool AddBinding(Binding* l_binding);
+    bool AddBinding(Binding *l_binding);
     bool RemoveBinding(std::string l_name);
+
     void SetFocus(const bool& l_focus);
 
-    /// The callback.
     template<class T>
-    bool AddCallback(const std::string& l_name, void(T::*l_func) (EventDetails*), T* l_instance)
+    bool AddCallback(std::string& l_name, void(T::*l_func)(EventDetails*), T* l_instance)
     {
         auto temp = std::bind(l_func, l_instance, std::placeholders::_1);
         return m_callbacks.emplace(l_name, temp).second;
     }
 
-    void RemoveCallback(const std::string& l_name)
+    void RemoveCallback(std::string& l_name)
     {
         m_callbacks.erase(l_name);
     }
@@ -113,7 +115,6 @@ public:
     {
         return (l_wind ? sf::Mouse::getPosition(*l_wind) : sf::Mouse::getPosition());
     }
-
 private:
     void LoadBindings();
 
